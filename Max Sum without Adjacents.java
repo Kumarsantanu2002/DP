4326bc3ba6947a81453cc169927ff4db0ef9292b
Kumarsantanu2002 <158434127+Kumarsantanu2002@users.noreply.github.com>
2024-08-25 20:58:50 +0530

    Update Max Sum without Adjacents.java

--- Max Sum without Adjacents.java
+++ Max Sum without Adjacents.java
@@ -54,3 +54,33 @@ class Solution {
         return dp[n-1];
     }   
 }
+
+==========================
+    class Solution {
+    int findMaxSum(int arr[], int n) {
+        if(n==1){
+            return arr[0];
+        }
+        if(n==2){
+            return Math.max(arr[0],arr[1]);
+        }
+        
+       int prev1=arr[0];
+       int prev2=0;
+        for(int i=1;i<n;i++)
+        {
+            int fs=arr[i];
+            if(i>1){
+                fs+=prev2;
+            }
+            int ss=prev1;
+            
+            int temp=Math.max(fs,ss);
+            prev2=prev1;
+            prev1=temp;
+        }
+        return prev1;
+    }
+    
+    
+}
